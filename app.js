9d0aa3c17ee51776c6f054573ee339668a592efa
Joshua Kelly <joshuakellyeng@gmail.com>
2022-01-12 15:14:31 -0500

    still figuring how to append the new item

--- app.js
+++ app.js
@@ -1,34 +1,59 @@
-//grocery receipts array
-const supplies = [
-	{
-		item: 'Bath Tissue',
-		brand: 'Kirkland',
-		units: '36 Rolls',
-		quantity: 1,
-		price: 26.99,
-		isPurchased: false,
-	},
-	{
-		item: 'Paper Towels',
-		brand: 'Kirkland',
-		units: '12 Rolls',
-		quantity: 1,
-		price: 19.99,
-		isPurchased: false,
-	},
-	{
-		item: 'Signature Napkins',
-		brand: 'Kirkland',
-		units: '1120 Napkins',
-		quantity: 1,
-		price: 11.49,
-		isPurchased: false,
-	},
-];
-
 class App extends React.Component {
+	state = {
+		supplies: suppliesList,
+		item: '',
+		units: '',
+		quantity: 0,
+		price: 0,
+	};
+
+	//arrow function
+	//handle change 
+	handleChange = (e) => {
+		this.setState({
+			[e.target.id]: e.target.value,
+		});
+	};
+
+	handleSumbit = e => {
+		e.preventDefault();
+	
+	}
+
 	render() {
-		return <h1>Hello, we are connected!</h1>;
+		return (
+			<div class="ui segment">
+				<h1>RostCo Wholesale</h1>
+				<div id="new-item">
+					<form class="ui form">
+						<label htmlFor="item">Item:</label>
+						<input
+							id="item"
+							type="text"
+							value={this.state.item}
+							onChange={this.handleChange}
+						/>
+						<label htmlFor="units">Units:</label>
+						<input id="units" type="text" value={this.state.units} onChange={this.handleChange}  />
+						<label htmlFor="quantity">Quantity:</label>
+						<input id="quantity" type="text" value={this.state.quantity} onChange={this.handleChange} />
+						<button>
+							<i class="shop icon"></i>
+						</button>
+					</form>
+				</div>
+
+				<div class="supplies">
+					{this.state.supplies.map((supply) => (
+						<div class="ui segment">
+							<h1>Item: {supply.item}</h1>
+							<h2>Units: {supply.units}</h2>
+							<h4>Qty: {supply.quantity}</h4>
+						</div>
+					))}
+				</div>
+			</div>
+		);
 	}
 }
 
